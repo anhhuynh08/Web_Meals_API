3bbd9669d2e2ae3d414735903fa8490e1e0585ab
huynhanh08 <anhhuynh0898@gmail.com>
2021-06-07 08:29:33 +0700

    update-layout

--- src/components/Meal.js
+++ src/components/Meal.js
@@ -7,7 +7,7 @@ class Meal extends React.Component {
     state = {
         activeMeal: []
     }
-    
+
     componentDidMount = async () => {
         const idm = this.props.match.params.id;
         const req = await fetch
@@ -19,15 +19,15 @@ class Meal extends React.Component {
     render() {
         const me = this.state.activeMeal;
         return (
-            <div className="container mx-auto">
+            <div className="container ">
                 {
                     this.state.activeMeal.length !== 0 &&
                     <div className="row mt-2">
                         <div className="col col-md-7 col-sm-12 mb-2">
-                            <div className="active_meal text-center mx-auto">
+                            <div className="active_meal text-center">
                                 <img alt={me.strMeal} className="img-fluid active-meal mb-2 " src={me.strMealThumb} />
                                 <div className="d-block d_youtube" >
-                                    <ReactPlayer  url={me.strYoutube} />
+                                    <ReactPlayer url={me.strYoutube} />
                                 </div>
                             </div>
                         </div>
@@ -35,7 +35,8 @@ class Meal extends React.Component {
                             <h3 className="active_meal title text-danger">{me.strMeal}</h3>
                             <h4 className="active_meal category">Category: <span>{me.strCategory}</span></h4>
                             <h5 className="active_meal area">Area: <span>{me.strArea}</span></h5>
-                            <p className="active_meal website">Website:<span><a href={me.strSource}>{me.strSource}</a></span></p>
+                            <p className="active_meal website">Website:
+                            <span><a className="btn btn-primary ml-2" href={me.strSource}> Link here</a></span></p>
                             <p className="active_meal text-justify instructions">Instructions:<span> {me.strInstructions}</span></p>
                             <button className="active_meal btn-danger mt-3">
                                 <Link to="/">Go home</Link>
